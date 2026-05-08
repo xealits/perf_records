a01a6c253b9f6a6e0de846ffa79898ef116130d5
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2026-05-08 02:13:09 +0200

    An implementation of requesting a RecordStd from Counters
    RecordStd is a type that is composed of language-standard types. The
    goal is to easily pass it between components of the program, with no
    custom types and headers. It should be easy to convert it to whatever
    other types. And it is not performance-critical.
    
    The Counters::nametree_to_record_std generates a RecordStd by getting
    the templated counters inside the nested Counters according to a given
    typename Nodetree, which is a nested tree of counter names. Ideally,
    this should be done automatically with Cpp26 reflections. When the
    reflections reliably show up, it is worth to revisit and try to
    implement an automatic reflections-based member function.
    
    	modified:   counters.hpp
    	new file:   records_std.hpp

--- counters.hpp
+++ counters.hpp
@@ -1,6 +1,11 @@
 #pragma once
+#ifndef PERF_RECORDS_NAMESPACE_NAME
+#define PERF_RECORDS_NAMESPACE_NAME perf_records
+#endif
 
-namespace perf_records {
+#include "records_std.hpp"
+
+namespace PERF_RECORDS_NAMESPACE_NAME {
 
 template<unsigned str_size>
 struct FixedString {
@@ -30,15 +35,65 @@ struct FixedString {
 
         return true;
     }
+
+    // strict ordering is needed to use FixedString as a key in std::map
+    bool operator<(const FixedString<str_size>& other) const {
+        // compare up to the first \0 character or the end of strings
+        for (size_t chi = 0; chi < str_size; chi++) {
+            if (value[chi] != other.value[chi]) {
+                return value[chi] < other.value[chi];
+            }
+
+            if (value[chi] == '\0' && other.value[chi] == '\0') {
+              // the two strings are identical
+              return false;
+            }
+
+            if (value[chi] == '\0' || other.value[chi] == '\0') {
+                return value[chi] == '\0';
+            }
+        }
+
+        // the two strings are identical
+        return false;
+    }
+
+    //
+    friend std::ostream& operator<<(std::ostream& outs, const FixedString<str_size>& fstr) {
+        outs << fstr.c_str();
+        return outs;
+    }
+
+    // also convert to std::string
+    operator std::string(void) const {
+        return std::string{value, value + str_size};
+    }
 };
 
 using CounterNameT = FixedString<32>;
 
+template<typename... Ts>
+struct TypePack {};
+
+template<typename... Ts>
+constexpr unsigned sizeof_pack(TypePack<Ts...> pack) {
+  return sizeof...(Ts);
+}
+
+// the manual way to pull the info from the templated class
+// by a given nested tree of template parameters
+// ideally, this should be done with Cpp26 reflections
+template<CounterNameT name, typename... Subnodes>
+struct NameTreeNode {
+  static constexpr inline CounterNameT node_name = name;
+  static constexpr inline TypePack<Subnodes...> subnodes_t{};
+};
+
 template<typename CounterDataT
     , auto CallableInputProc = [](const auto& inp) {return inp;}
-    // WARNING: an auto parameter in a lambda in a template is a bug in GCC 14 as of 2024-07:
-    // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=88313
+    // WARNING: an auto parameter in a lambda in a template is a bug in GCC 14
     // it is fixed in GCC 14.3
+    // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=88313
     , auto CallableCurrentCounter = [](void) {} /// default is intentionally broken
     >
 
@@ -88,6 +143,69 @@ struct Counters {
 
     template<CounterNameT name>
     using counter = DataStruct<name>;
+
+    // helper to convert the counters data to an std records structure
+    template<typename ParentCounters, typename OutMapNameT, typename Subnode, typename... Rest>
+    static void add_subnodes_to_map(std::map<OutMapNameT, RecordStd<CounterDataT>>& nodes_map, const TypePack<Subnode, Rest...> pack) {
+        {
+            // add the Subnode and its nested tree if present
+            RecordStd<CounterDataT> rec;
+            // get the subnode data from the parent type
+            rec.data = ParentCounters::template counter<Subnode::node_name>::data;
+
+            if constexpr (sizeof_pack(Subnode::subnodes_t) > 0) {
+                using sub_counters_t = ParentCounters::template counter<Subnode::node_name>::sub_counters;
+                add_subnodes_to_map<sub_counters_t>(rec.sub_records, Subnode::subnodes_t);
+            }
+            nodes_map[Subnode::node_name] = rec;
+        }
+
+        // add the Rest subnodes
+        constexpr auto rest_size = sizeof...(Rest);
+        if constexpr (rest_size > 0) {
+            add_subnodes_to_map<ParentCounters>(nodes_map, TypePack<Rest...>{});
+        }
+    }
+
+    template<typename NodeCounter, typename... Subnodes>
+    static RecordStd<CounterDataT> nametree_to_record_std(const TypePack<Subnodes...>& pack) {
+        CounterDataT rec_data = NodeCounter::data;
+
+        if constexpr (sizeof...(Subnodes) == 0) {
+            return RecordStd<CounterDataT>{rec_data};
+        }
+
+        else {
+            RecordStd<CounterDataT> rec_tree;
+            rec_tree.data = rec_data;
+
+            using sub_counters_t = NodeCounter::sub_counters;
+            add_subnodes_to_map<sub_counters_t>(rec_tree.sub_records, TypePack<Subnodes...>{});
+            return rec_tree;
+        }
+    }
+
+    template<typename Nodetree, typename... OtherNodetrees>
+    static RecordStd<CounterDataT> nametree_to_record_std(void) {
+        RecordStd<CounterDataT> top_counters_record; // TODO: currently Counters is a bit abnormal:
+        // it contains counter<name> template
+        // which instantiates a tree of subnodes
+        // so Counters is unlike its subnodes, it is a set of these subnode trees
+        // probably I should converge them - give Counters an ID name and turn it into a subnode tree itself
+        // it makes sense, because the same data-related type parameters (data type and two processing lambdas)
+        // can be used in a bunch of different areas of the code (the timing counters e.g.)
+
+        using CounterT = Counters<CounterDataT, CallableInputProc, CallableCurrentCounter>::counter<Nodetree::node_name>;
+        auto rec = nametree_to_record_std<CounterT>(Nodetree::subnodes_t);
+        top_counters_record.sub_records[Nodetree::node_name] = rec;
+
+        if constexpr (sizeof...(OtherNodetrees) > 0) {
+            auto other_top_rec = nametree_to_record_std<OtherNodetrees...>();
+            top_counters_record.sub_records.insert(other_top_rec.sub_records.begin(), other_top_rec.sub_records.end());
+        }
+
+        return top_counters_record;
+    }
 };
 
 };
---
+++ records_std.hpp
@@ -0,0 +1,29 @@
+#pragma once
+// serialisation into language standard types
+// for easier and faster building and linkage
+
+#ifndef PERF_RECORDS_NAMESPACE_NAME
+#define PERF_RECORDS_NAMESPACE_NAME perf_records
+#endif
+
+#include <string>
+#include <map>
+
+namespace PERF_RECORDS_NAMESPACE_NAME {
+
+template<typename DataT>
+struct RecordStd {
+  DataT data{};
+  std::map<std::string, RecordStd<DataT>> sub_records{};
+
+  void print(const std::string& name, unsigned nesting = 0) const {
+    for (unsigned ind = 0; ind < nesting; ind++) std::cout << " ";
+    std::cout << "RecordStd " << name << " = " << data << '\n';
+
+    for (const auto& [name, rec_std] : sub_records) {
+      rec_std.print(name, nesting+1);
+    }
+  }
+};
+
+};
